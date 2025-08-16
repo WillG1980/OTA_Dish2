820568faa7c6cbb57f73024928ccfece9d73c49c
will@sjcnu.com <will@sjcnu.com>
2025-08-16 18:44:52 -0400

    Auto-commit on build: Sat 08/16/2025 18:44:52.70

--- esp_build.bat
+++ esp_build.bat
@@ -28,7 +28,7 @@ if errorlevel 1 (
 @echo on
 set WEB_FIRMWARE="Y:\firmware\ota-dishwasher\%VERSION%.bin"
 rem mkdir %WEB_FIRMWARE%
-copy C:\Projects\esp\OTA_DISHWASHER\build\ota-dishwasher.bin  %WEB_FIRMWARE%
-#idf.py flash monitor
+copy C:\Projects\esp\OTA_DISH2\build\ota-dishwasher.bin  %WEB_FIRMWARE%
+idf.py flash monitor
 
 
