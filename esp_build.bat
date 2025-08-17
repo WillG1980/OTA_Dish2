abf365b94671beb11d3dd7ccf9c4a2d4f355ad36
will@sjcnu.com <will@sjcnu.com>
2025-08-17 12:43:38 -0400

    Auto-commit on build: Sun 08/17/2025 12:43:38.27

--- esp_build.bat
+++ esp_build.bat
@@ -1,30 +1,20 @@
 @echo off
-
+echo get new git version
+FOR /F %%i IN ('git rev-list --count HEAD') DO SET /a VERSION=%%i+1
+echo Determined new version to be: %VERSION%, testing the build
 idf.py build
 if errorlevel 1 (
-    echo Error detected, exiting...
+    echo Error detected, exiting, no GIT commit
     exit /b 1
 )
-
-echo Add new files
+echo Add new files to GIT
 git add .
 echo Commit changed files
 git diff --cached --quiet || (
     git commit -m "Auto-commit on build: %DATE% %TIME%" 
     FOR /F %%i IN ('git rev-list --count HEAD') DO git tag -a build-%%i -m "Build tag"
 )
-REM Get the version as the commit count
-echo get new git version
-FOR /F %%i IN ('git rev-list --count HEAD') DO SET VERSION=%%i
-echo Determined version to be: %VERSION%
-REM Replace placeholder in version.h.in and write to version.h
-REM echo set version # in string
-REM powershell -Command "(Get-Content version.h.in) -replace '@VERSION@', '%VERSION%' | Set-Content version.h"
-idf.py build
-if errorlevel 1 (
-    echo Error detected, exiting...
-    exit /b 1
-)
+echo Copy files to firmware update repository
 @echo on
 set WEB_FIRMWARE="Y:\firmware\ota-dishwasher\%VERSION%.bin"
 rem mkdir %WEB_FIRMWARE%
