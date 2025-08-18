ead1fba26a4dd859dc5176993c742b7f2dd51071
will@sjcnu.com <will@sjcnu.com>
2025-08-17 21:15:14 -0400

    Auto-commit on build: Sun 08/17/2025 21:15:14.49

---
+++ main/local+partitions.h
@@ -0,0 +1,11 @@
+#include "esp_ota_ops.h"
+#include "esp_partition.h"
+#include <stdio.h>
+#include <string.h>
+
+
+
+static inline void boot_partition_string(char *out, size_t out_sz);
+static inline const char* boot_partition_cstr(void);
+static inline void running_partition_string(char *out, size_t out_sz);
+static inline const char* running_partition_cstr(void);
