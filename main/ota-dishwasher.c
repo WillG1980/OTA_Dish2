b117cc743c82fe14560ff390ed2a9dc4f5c02440
will@sjcnu.com <will@sjcnu.com>
2025-08-17 23:17:12 -0400

    Auto-commit on build: Sun 08/17/2025 23:17:12.00

--- main/ota-dishwasher.c
+++ main/ota-dishwasher.c
@@ -292,12 +292,14 @@ static void run_program(void *pvParameters) {
     COPY_STRING(ActiveStatus.Step, Line->name_step);
 
     _LOG_I("%s:%s->%d: GPIO-mask %lld\n", ActiveStatus.Program,Line->name_cycle, Line->name_step, TTR,return_masked_bits(Line->gpio_mask, ALL_ACTORS));
+    gpio_mask_set(Line->gpio_mask); // set all pins to off
     vTaskDelay(pdMS_TO_TICKS(5 * SEC)); // run for 5 seconds minimum
 
     for (; TTR > 0; TTR -= 5000) 
         {
           gpio_mask_set(Line->gpio_mask); // set all pins to off
           _LOG_I("\t\t%s:%s\t%d",Line->name_cycle,Line->name_step,TTR);
+          vTaskDelay(pdMS_TO_TICKS(5000));
         }
     
     
